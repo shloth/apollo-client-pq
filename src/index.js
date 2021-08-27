04df73a572fde96d3b90d15ade23323b142d2be6
Saad Farooq <shloth@Saads-MBP.fios-router.home>
2021-08-27 16:39:32 -0400

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
+import {
+    ApolloProvider,
+    useQuery,
+    gql
+  } from "@apollo/client";
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import { createPersistedQueryLink } from "apollo-link-persisted-queries";
+import { ApolloClient, ApolloLink, InMemoryCache } from "@apollo/client";
+import { HttpLink } from "apollo-link-http";
+import { sha256 } from 'crypto-hash';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ 
+const EXCHANGE_RATES = gql`
+query GetExchangeRates {
+  rates(currency: "USD") {
+    currency
+    rate
+  }
+}
+`;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function ExchangeRates() {
+const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+if (loading) return <p>Loading...</p>;
+if (error) return <p>Error :(</p>;
+
+return data.rates.map(({ currency, rate }) => (
+  <div key={currency}>
+    <p>
+      {currency}: {rate}
+    </p>
+  </div>
+));
+}
+
+  const linkChain = createPersistedQueryLink({ sha256, useGETForHashedQueries: true }).concat(
+  new HttpLink({ uri: "http://localhost:4000/graphql" }));
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: linkChain,
+    connectToDevTools: true
+    
+  });
+
+  function App() {
+    return (
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+      </div>
+    );
+  }
+  
+  render(
+    <ApolloProvider client={client}>
+      <App />
+      <ExchangeRates />
+    </ApolloProvider>,
+    document.getElementById('root'),
+  );
+
+ 
